4d212908d736cf455d769215bd83809141c4d0d1
EthanNValencia <enephew2@mail.valenciacollege.edu>
2023-07-27 16:00:32 -0400

    Updating for docker compose.

--- src/main/java/com/nephew/microservices/currencyconversionservice/RestTemplateConfiguration.java
+++ src/main/java/com/nephew/microservices/currencyconversionservice/RestTemplateConfiguration.java
@@ -5,6 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+
+/***
+ * It is necessary to construct the RestTemplate through a bean in order
+ * to see the entire tracing event in zipkin. 
+ * @author Ethan
+ * 
+ * URL: http://localhost:8765/currency-conversion/from/USD/to/INR/quantity/10
+ */
 @Configuration(proxyBeanMethods = false)
 public class RestTemplateConfiguration {
 	
